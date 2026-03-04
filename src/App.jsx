f531b281e88f35e2c301a35e00b028992ff6797f
priyanshu-k32 <keshripriyanshuin@gmail.com>
2026-03-04 10:18:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  const [user,setUser] = useState(null);
+
+  const getUser = async () => {
+    const res = await fetch("https://randomuser.me/api/");
+    const data = await res.json();
+    setUser(data.results[0]);
+  };
+
+  return (
+    <div>
+      <button onClick={getUser}>Get User</button>
+      {user && (
+        <div>
+          <img src={user.picture.large}/>
+          <h2>{user.name.first} {user.name.last}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
